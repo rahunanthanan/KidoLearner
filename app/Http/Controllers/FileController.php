6f36842999f62c01247f3e11f6a5df12339269ed
Jeyamaal <rcholan11@gmail.com>
2016-02-25 19:07:33 +0530

    25-02-2016  7.05 pm updated  Updated FileController

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -3,18 +3,17 @@
 use App\Http\Controllers\Controller;
 use App\Fileentry;
 use Request;
-
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\File;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Input;
+use Alert;
+use UxWeb\SweetAlert\SweetAlert;
 
 class FileController extends Controller {
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return Response
-     */
+
     public function index()
     {
         $entries = Fileentry::all();
@@ -24,18 +23,23 @@ class FileController extends Controller {
 
     public function add() {
 
-        $file = Request::file('filefield_file');
-        $extension = $file->getClientOriginalExtension();
-        Storage::disk('local')->put($file->getFilename().'.'.$extension,  File::get($file));
-        $entry = new Fileentry();
-        $entry->mime = $file->getClientMimeType();
-        $entry->original_filename = $file->getClientOriginalName();
-        $entry->filename = $file->getFilename().'.'.$extension;
+        if (Input::hasFile('filefield')) {
 
-        $entry->save();
+            $file = Request::file('filefield');
+            $extension = $file->getClientOriginalExtension();
+            Storage::disk('local')->put($file->getFilename() . '.' . $extension, File::get($file));
 
-        return redirect('fileentry');
+            $entry = new Fileentry();
+            $entry->mime = $file->getClientMimeType();
+            $entry->original_filename = $file->getClientOriginalName();
+            $entry->filename = $file->getFilename() . '.' . $extension;
 
+            $entry->save();
+
+            Alert::success('Successfully Added');
+
+            return redirect('viewentry');
+        }
     }
 
     public function get($filename){
