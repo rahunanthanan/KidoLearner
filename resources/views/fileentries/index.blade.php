d4ad0b49ef5cb0b277f04d8afaa9e0ffbb8ab2d3
Jeyamaal <rcholan11@gmail.com>
2016-02-25 19:02:39 +0530

    25-02-2016  7.01 pm updated  fileentries/index.blade.php deleted show.blade

--- resources/views/fileentries/index.blade.php
+++ resources/views/fileentries/index.blade.php
@@ -29,25 +29,58 @@
 
 @section('content')
 
-    <form class="form-vertical" enctype="multipart/form-data" method="post" action="{{ url('getentry') }}"/>
+    <script src="dist/sweetalert.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="dist/sweetalert.css">
+
+
+   {{-- <form class="form-vertical" enctype="multipart/form-data" method="post" action="{{ url('addentry') }}"/>--}}
+{{--    <form class="form-vertical" enctype="multipart/form-data" method="post" action="{{ url('getentry') }}"/>--}}
+{{--
     <input type="file" name="filefield">
     <input type="submit">
-</form>
 
-<h1> Pictures list</h1>
-<div class="row">
-    <ul class="thumbnails">
-        @foreach($entries as $entry)
-            <div class="col-md-2">
-            {{--    <div class="thumbnail">
-                    <img src="{{route('getentry', $entry->filename)}}" alt="ALT NAME" class="img-responsive" />--}}
-                    <div class="caption">
-                        <p>{{$entry->original_filename}}</p>
+    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+    </form>
+
+    <h1> Pictures list</h1>
+
+    <div class="row">
+
+        <ul>
+            @foreach($entries as $entry)
+                <li>{{$entry->original_filename}}</li>
+            @endforeach
+        </ul>
+    </div>
+
+@endsection
+--}}
+
+
+
+
+
+
+    <form action="{{url('addentry', [])}}" method="post" enctype="multipart/form-data">
+        <input type="file" name="filefield">
+        <input type="submit">
+    </form>
+
+    <h1> Pictures list</h1>
+    <div class="row">
+        <ul class="thumbnails">
+            @foreach($entries as $entry)
+                <div class="col-md-2">
+                    <div class="thumbnail">
+                        <img src="{{url('getentry',$entry->filename)}}" alt="ALT NAME" class="img-responsive" />
+                        <div class="caption">
+                            <p>{{$entry->original_filename}}</p>
+                        </div>
                     </div>
                 </div>
-            {{--</div>--}}
-        @endforeach
-    </ul>
-</div>
+            @endforeach
+        </ul>
+    </div>
 
 @endsection
