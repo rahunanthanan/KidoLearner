94716659318c68cef263d7c84caa405651b35c33
Jeyamaal <rcholan11@gmail.com>
2016-02-25 18:42:57 +0530

    25-02-2016  6.40 pm updated  courses/create.blade.php

--- resources/views/courses/create.blade.php
+++ resources/views/courses/create.blade.php
@@ -2,19 +2,27 @@
 
 @section('content')
 
+        <!--  jQuery -->
+        <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.0/jquery.min.js"> </script>
+        <!-- Isolated Version of Bootstrap, not needed if your site already used Bootstrap -->
+        <link rel="stylesheet" href="https://formden.com/static/cdn/bootstrap-iso.css" />
+
+        <!-- Bootstrap Date-Picker Plugin -->
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
+
+
+
+
 
     <h1>Create Lesson</h1>
-    {{--{!! Form::open(['url'=>'courses'],['files=>true'])!!}--}}
-    <form class="form-vertical" enctype="multipart/form-data" method="post" action="{{ url('category') }}"/>
+
+    <form class="form-vertical" enctype="multipart/form-data" method="post" action="{{ url('courses') }}"/>
 
 
-    @if(Session::has('flash_category_empty'))
-        <script>
-            swal({title: "Error!",   text: "Empty Filed!",type: "error", confirmButtonText: "Cool" });
-        </script>
-
-
-    @endif
 
     <div class="form-group">
         {!! Form::label('name','Name:') !!}
@@ -22,11 +30,101 @@
     </div>
 
 
+    @if ($errors->has('name')) <p class="help-block" style="color:red">{{ $errors->first('name') }}</p> @endif
+
+
+
+     <div class="form-group">
+        <label for="">Group</label>
+        <select class="form-control input-sm" name="category" id="category">
+
+                        @foreach($categories as $category)
+
+                            <option value="{{$category->id}}">{{$category->name}}</option>
+
+                        @endforeach
+         </select>
+
+
+    </div>
+    <div class="form-group">
+        <label for="">Activity</label>
+        <select class="form-control input-sm" name="subcategory" id="subcategory">
+
+
+
+                <option value=""></option>
+
+
+        </select>
+
+
+    </div>
+
+
+
+    <div class="form-group">
+        {!! Form::label('Description','Description:') !!}
+        {!! Form::textarea('description',null,['class'=>'form-control','size' => '5x5']) !!}
+    </div>
+
+    @if ($errors->has('description')) <p class="help-block" style="color:red">{{ $errors->first('description') }}</p> @endif
+
+
+    <div class="form-group">
+    {!! Form::label('date', 'UploadDate:') !!}
+
+        <input type="date"  name="date" value="<?php echo date('Y-m-d'); ?>" />
+    </div>
+
+    <br><br>
+
+    <div class="form-group">
+
+        <label for="filefield">Upload Image</label>
+        <input type='file' name="filefield" />
+        <input type="hidden" value="{{ csrf_token() }}" name="_token">
+
+    </div>
+
+
+    @if ($errors->has('filefield')) <p class="help-block" style="color:red">{{ $errors->first('filefield') }}</p> @endif
+
+
     <div class="form-group">
         {!! Form::submit('Save',['class' => 'btn btn-success']) !!}
     </div>
-    <input type="hidden" value="{{ csrf_token() }}" name="_token">
+
+    <script>
+
+        $('#category').on('change',function(e){
+
+           console.log(e);
+           var cat_id= e.target.value;
+
+            $.get('/ajax-subcat?cat_id=' + cat_id,function(data){
+
+                console.log(data);
+                $('#subcategory').empty();
+
+                $.each(data,function(index,subcatObj){
 
 
-    {!! Form::close() !!}
+                    $('#subcategory').append('<option value="'+subcatObj.id+'">'+subcatObj.name+ '</option>');
+
+                });
+
+            });
+
+
+
+        });
+
+
+
+    </script>
+
+
+
+{!! Form::close() !!}
 @stop
